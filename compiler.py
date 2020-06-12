a57fb693b09a9d07b57a0a2d6caca0f28d6c0f80
UC1309 <arxliu@gmail.com>
2020-06-11 23:39:00 -0500

    final commenting added, ready for submission

--- compiler.py
+++ compiler.py
@@ -1,36 +1,16 @@
-#%%
+# Imports
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
 import os
 
-
-#%%
-dataDir = "data/"
-growth_rate = pd.read_csv(dataDir + "covid_clean.csv", encoding="latin_1")
-growth_rate
-
-
-#%%
-job_shares = pd.read_csv(dataDir + "jobs_shares.csv", encoding="latin_1")
-num_fips = []
-for fip in job_shares['geofips']:
-    num_fips.append(int(fip.strip('"')))
-job_shares['geofips'] = num_fips
-job_shares = job_shares.rename(columns={"geofips" : 'fips'})
-
-
-#%%
-lockdown = pd.read_csv(dataDir + "lockdown_dates.csv")
-us_lock = lockdown[lockdown['Country'] == 'United States']
-state2fips = pd.read_csv(dataDir + "state_fips.csv").rename(columns={"stname" : "Place"})
-state2fips["statefips"] = state2fips[' st'] * 1000
-state2fips = state2fips.drop([' st', ' stusps'], axis = 1)
-
+''' export2csv creates a .csv file for a given data frame
+If the file exists, it deletes the existing file
+'''
 def export2csv(filedir, filename, data):
     if os.path.exists(filedir + filename):
         os.remove(filedir + filename)
@@ -39,10 +19,28 @@ def export2csv(filedir, filename, data):
         print("The file does not exist")
     data.to_csv(filedir + filename, index=False, encoding='latin_1')
 
+# Load necessary csv files
+dataDir = "data/"
+growth_rate = pd.read_csv(dataDir + "covid_clean.csv", encoding="latin_1")
+job_shares = pd.read_csv(dataDir + "jobs_shares.csv", encoding="latin_1")
+
+# Convert FIPS strings to numerical type
+num_fips = []
+for fip in job_shares['geofips']:
+    num_fips.append(int(fip.strip('"')))
+job_shares['geofips'] = num_fips
+job_shares = job_shares.rename(columns={"geofips" : 'fips'})
+
+# Load lockdown data and create a state-to-fips mapping
+lockdown = pd.read_csv(dataDir + "lockdown_dates.csv")
+us_lock = lockdown[lockdown['Country'] == 'United States']
+state2fips = pd.read_csv(dataDir + "state_fips.csv").rename(columns={"stname" : "Place"})
+state2fips["statefips"] = state2fips[' st'] * 1000
+state2fips = state2fips.drop([' st', ' stusps'], axis = 1)
+
 export2csv(dataDir, "state_fips_clean.csv", state2fips)
 
-
-#%%
+# Gathers the population for every county
 population = pd.read_csv(dataDir + "dollars_clean.csv", encoding = "latin_1")
 population = population[['GeoFIPS', '20']]
 population['20'] = population['20'].astype(int)
@@ -54,23 +52,31 @@ population = population.rename(columns={"GeoFIPS" : 'fips', "20" : 'population'}
 
 export2csv(dataDir, "population.csv", population)
 
+# Gets the 2010 County Land Area Census data and calculates population density.
+area = pd.read_excel(dataDir + "county_area.xls", encoding = "latin_1")
+'''
+LND110210D is the code for Land Area data for the year 2010. 
+Check Census website for naming convention. STCOU are the FIPS.
+Area is measured in square miles.
+'''
+area = area[["STCOU", "LND110210D"]]
+area = area.rename(columns = {"STCOU" : "fips", "LND110210D" : "area"})
+density = pd.merge(area, population, on="fips")
+density['density'] = density['population'] / density['area']
 
-#%%
+export2csv(dataDir, "density.csv", density)
+
+# Necessary data merging to create the regression data
 fips_lockdown = pd.merge(us_lock, state2fips, on="Place")[['statefips', 'Start date']]
 fips_lockdown = fips_lockdown.rename(columns={"Start date" : "lockdown start"})
 fips_lockdown = fips_lockdown.sort_values('statefips')
 
 export2csv(dataDir, "us_lockdown_dates.csv", fips_lockdown)
 
-
-#%%
 temp = pd.merge(growth_rate, fips_lockdown, on='statefips', how = 'outer')
 temp['lockdown_delta'] = (pd.to_datetime(temp['lockdown start']) - pd.to_datetime(temp['start_date'])).dt.days
-temp = pd.merge(temp, population, on='fips')
-#temp[temp['fips'] == 53033].head(10)#
+temp = pd.merge(temp, density[["fips", "density"]], on='fips')
 
-
-#%%
 export = pd.merge(temp, job_shares, on='fips')
 drop_cols = ['statefips', 'start_date', 'lockdown start']
 for idx, col in enumerate(job_shares.columns):
@@ -79,13 +85,3 @@ for idx, col in enumerate(job_shares.columns):
 export = export.drop(columns = drop_cols)
 
 export2csv(dataDir, "regression_data.csv", export)
-
-
-#%%
-list(enumerate(export.columns))
-
-
-#%%
-
-
-
--- regressors.py
+++ regressors.py
@@ -1,29 +1,30 @@
-#%%
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
 import os
 
-
-#%%
+# Loads the jobs data
 dataDir = "data/"
-df = pd.read_csv(dataDir + "jobs.csv", encoding="latin_1")
+df = pd.read_csv(dataDir + "jobs_raw.csv", encoding="latin_1")
 for idx, sector in enumerate(df['Description']):
     if sector != sector:
         df = df.drop(idx)
 
-
-#%%
+# Creates a blank data frame with a column for every industry type
 sectors = df['Description'].unique().tolist()
 for idx, sector in enumerate(tqdm(sectors)):
     sectors[idx] = sector.strip().lower()
 col_names = np.insert(sectors, 0, ['geofips'])
 data = pd.DataFrame(columns=col_names)
 
+''' This block of code gathers the 2018 industry data, cleans null
+values, and then fills our blank data frame with job data for each
+county.
+'''
 df = df[['GeoFIPS', 'Description', '2018']]
 fips = df['GeoFIPS'].unique()
 a = {}
@@ -42,13 +43,12 @@ for fip in tqdm(a.keys()):
         new_row[sector] = jobs
     data = data.append(new_row, ignore_index=True)
 
-
-#%%
+# Some counties exist with no employment data. We remove these
 data = data[data['total employment (number of jobs)'] != 0]
-data
+# Removes the other sector to remove multicolinearity
+data = data.drop(columns=['other services (except government and government enterprises)'])
 
-
-#%%
+# csv export for raw job numbers
 if os.path.exists(dataDir + "jobs_clean.csv"):
     os.remove(dataDir + "jobs_clean.csv")
     print("File deleted")
@@ -56,69 +56,20 @@ else:
     print("The file does not exist")
 data.to_csv(dataDir + 'jobs_clean.csv', index=False, encoding='latin_1')
 
-
-#%%
+# We extract the numerical data and the job totals to prepare the share calculation
 job_numbers = data.drop(labels = 'geofips', axis = 1).to_numpy()
 total, subtotals = np.split(job_numbers, [1], axis=1)
 geofips = data['geofips'].to_numpy().reshape((-1, 1))
 
-
-#%%
-
-
-
-#%%
+# We calculate the shares using vector operations for efficiency
 sector_shares = np.hstack((geofips, total, subtotals / total))
+col_names = data.columns
 sector_shares = pd.DataFrame(sector_shares, columns = col_names)
-sector_shares
 
+# csv export for job shares
 if os.path.exists(dataDir + "jobs_shares.csv"):
     os.remove(dataDir + "jobs_shares.csv")
     print("File deleted")
 else:
     print("The file does not exist")
 sector_shares.to_csv(dataDir + 'jobs_shares.csv', index=False, encoding='latin_1')
-
-#%% [markdown]
-# 218 is Monterey, CA (large ass aquarium and fishing disctrict, perfectly reasonable)
-# 
-# 355 is Hendry, FL (which is home to the Okaloacoochee Slough State Forest and various state preserves)
-# 
-# 2462 is Sully, SD (right next to the Missouri River, so I guess tons of fishing)
-# 
-# 2722 is McMullen, TX (apparently richest county in Texas. Very small)
-# 
-# The only one that remotely makes sence is Monterey...
-
-#%%
-
-
-
-#%%
-
-
-#%% [markdown]
-# Publishing includes software publishing (so here's "big tech", so to speak)
-# 
-# 06075 is San Francisco, CA (omg what a shocker)
-# 
-# 06081 is San Mateo, CA (Sony is headquartered here)
-# 
-# 06085 is Santa Clara, CA (wow, I wonder what big tech companies are here)
-# 
-# 08014 is Broomfield, CO (apparently a telecomm company named Lvl 3 Communications. Also a small company names Oracle)
-# 
-# 13121 is Fulton Country, GA (it's Atlanta. Too many possibilities to count)
-# 
-# 36061 is New York, NY (I'm, uh, not gonna bother on this one)
-# 
-# 53033 is King County WA (This is home turf. Amazon, Microsoft, Steam, all here)
-
-#%%
-
-
-
-#%%
-
-
-
